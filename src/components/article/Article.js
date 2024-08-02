0b74486b668b8310dc85c892787e5bd8162b9645
한성준 <june123000@naver.com>
2024-08-03 04:23:38 +0900

    update: article 일단 완성, image 가져오는 부분은 추후 구현 예정

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,8 +1,26 @@
+import './Article.css';
+import leaves from '../../assets/images/leaves.jpg';
+import { ReactComponent as TimerIcon } from '../../assets/icons/timer.svg';
+
 function Article({ articleInstance }) {
     return (
-        <div>
-            <h1>Article</h1>
-        </div>
+        <article>
+            {/* <img src={articleInstance.image} alt={articleInstance.title} /> */}
+            <img src={leaves} alt={articleInstance.title} />
+            <section className='article-body'>
+                <span className='article-title'>{articleInstance.title}</span>
+                <section className='article-details'>
+                    <section className='article-info'>
+                        <span>{`${articleInstance.articleInfo.user.name} · ${articleInstance.articleInfo.createdAt} · `}</span>
+                        <span style={{ width: '20px', height: '20px' }}><TimerIcon /></span>
+                        <span>{articleInstance.articleInfo.readingTime.value} {articleInstance.articleInfo.readingTime.unit}</span>
+                    </section>
+                    <section className='article-data'>
+                        <span>{`views ${articleInstance.articledata.viewCnt} · comments ${articleInstance.articledata.commentCnt}`}</span>
+                    </section>
+                </section>
+            </section>
+        </article >
     );
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,13 +5,48 @@ import Nav from '../../components/nav/Nav';
 import CategoryAside from '../../components/categoryAside/CategoryAside';
 import Filter from './Filter';
 import Social from './Social';
+import Article from '../../components/article/Article';
 
 // css imports
 import './Main.css';
 
 function Main() {
+    const [posts, setPosts] = React.useState([]);
     useEffect(() => {
         console.log(`main page loaded`);
+        // fetch data from api
+        setPosts([
+            {
+                title: 'First Post',
+                articleInfo: {
+                    user: {
+                        id: 1,
+                        name: 'John Doe',
+                    },
+                    createdAt: '2021-01-01',
+                    readingTime: {
+                        value: 5,
+                        unit: 'm',
+                    },
+                },
+                articledata: {
+                    viewCnt: 1,
+                    commentCnt: 100,
+                },
+            },
+        ]);
+        // TODO - 백엔드 구현 후 다시 작성
+
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         setPosts(data);
+        //     })
+        //     .catch(error => {
+        //         console.error(error);
+        //     });
+
     }, []);
 
     return (
@@ -26,8 +61,9 @@ function Main() {
                     <section className='main-middle'>
                         <CategoryAside />
                         <section className='main-content'>
-                            <h2>Content</h2>
-                            <p>Content goes here</p>
+                            {posts.map((item) => (
+                                <Article key={item.id} articleInstance={item} />
+                            ))}
                         </section>
                         <Social />
                     </section>
