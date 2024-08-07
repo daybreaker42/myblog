cc4f43cbf44737bd8dc8b980492e88334cd9f608
한성준 <june123000@naver.com>
2024-08-08 01:53:44 +0900

    feat: Add @tanstack/react-query and query devtools

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={true} />
+    <App />
+  </QueryClientProvider >
 );
 
 // If you want to start measuring performance in your app, pass a function
