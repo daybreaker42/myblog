146fa396965fcac93b8756f39486b131db2044e6
한성준 <june123000@naver.com>
2024-08-13 23:51:53 +0900

    feat: 다른 페이지로 이동시 자동으로 페이지 맨 위로 스크롤되게 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Router from './router';
+import Router from './router/router';
 
 function App() {
   return (
--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Main from './pages/main/Main'
-import Profile from './pages/profile/Profile';
-import About from './pages/about/About';
-import Category from './pages/category/Category';
-import Tags from './pages/tags/Tags';
-import Settings from './pages/settings/Settings';
-import Article from './pages/article/Article';
+import Main from '../pages/main/Main'
+import Profile from '../pages/profile/Profile';
+import About from '../pages/about/About';
+import Category from '../pages/category/Category';
+import Tags from '../pages/tags/Tags';
+import Settings from '../pages/settings/Settings';
+import Article from '../pages/article/Article';
 
 // error pages
-import Error404 from './pages/errors/Error404';
+import Error404 from '../pages/errors/Error404';
+import ScrollToTop from './ScrollToTop';
 
 const Router = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path='/category' element={<Category />} />
@@ -20,7 +22,7 @@ const Router = () => {
                 <Route path='/about' element={<About />} />
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/settings' element={<Settings />} />
-                <Route path='/article/:id' element={<Article />} />
+                <Route path='/article/:slug' element={<Article />} />
                 <Route path='*' element={<Error404 />} />
             </Routes>
         </BrowserRouter>
