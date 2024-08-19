cad00ce822a4d0d924c68f5f86ea612185ea069a
한성준 <june123000@naver.com>
2024-08-19 23:08:21 +0900

    update: article card 및 menu 버그수정, 원래대로 원복

--- src/components/article/ArticleCard.js
+++ src/components/article/ArticleCard.js
@@ -1,18 +1,26 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';
+
+// import components
+import { ArticleMenu, ArticleMenuButton } from './ArticleMenu';
+
+
+// import utils
+import preventEvent from './utils/preventEvent';
+
 // import icons
 import { ReactComponent as TimerIcon } from '../../assets/icons/timer.svg';
 import { ReactComponent as MoreVertIcon } from '../../assets/icons/more_vert.svg';
 import { ReactComponent as CloseIcon } from '../../assets/icons/close.svg';
+
 // imports css
 import styles from './ArticleCard.module.css';
-// import image
-import leaves from '../../assets/images/leaves.jpg';
 
 /**
  * ArticleCard component
  * @param {Object} article - article object
  * @param {string} article.title - article title
+ * @param {string} article.content - article content
  * @param {Object} article.info - article information
  * @param {Object} article.info.user - user information
  * @param {string} article.info.user.name - user name
@@ -23,87 +31,54 @@ import leaves from '../../assets/images/leaves.jpg';
  * @param {Object} article.data - article data
  * @param {number} article.data.viewCnt - article view count
  * @param {number} article.data.commentCnt - article comment count
+ * @param {string} article.category - article category
+ * @param {Array<string>} article.tags - article tags
+ * 
+ * 
  * @returns {JSX.Element}
  */
 function ArticleCard({ article }) {
+    const [isLoading, setIsLoading] = useState(true);
     const [menuVisible, setMenuVisible] = useState(false);
-    const menuRef = useRef(null);
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
-                setMenuVisible(false);
-            }
-        };
-
-        if (menuVisible) {
-            document.addEventListener('mousedown', handleClickOutside);
-        } else {
-            document.removeEventListener('mousedown', handleClickOutside);
-        }
-
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [menuVisible]);
-
-    const toggleMenu = (event) => {
-        preventEvent(event);
-        setMenuVisible(!menuVisible);
-    };
-
-
 
     return (
         <Link to={`/article/${article.id}`}>
             <article className={styles.card}>
-
-                <img src={leaves} alt={article.title} className={styles['card-thumbnail']} />
+                <img loading='lazy' src={'https://picsum.photos/300/200'} alt={article.title} />
                 <div className={styles["card-content"]}>
-                    <h2 className={styles["card-title"]}>{article.title}</h2>
-                    <p className={styles["card-summary"]}>{article.content}</p>
-                    <div className={styles["card-meta"]}>
-                        <span className={styles["card-author"]}>작성자: {article.info.user.name}</span>
-                        <span className={styles["card-date"]}>{article.info.createdAt}</span>
+                    <div className={styles['card-top']}>
+                        <h2 className={styles["card-title"]}>
+                            {article.title}
+                        </h2>
+                        <ArticleMenuButton menuVisible={menuVisible} setMenuVisible={setMenuVisible} />
                     </div>
-                    <div className={styles["card-stats"]}>
-                        <span className={styles["card-read-time"]}>읽는 시간: {article.info.readingTime.value}{article.info.readingTime.unit}</span>
-                        <span className={styles["card-views"]}>조회수: {article.data.viewCnt}</span>
-                        <span className={styles["card-comments"]}>댓글: {article.data.commentCnt}</span>
+                    <p className={styles["card-summary"]}>
+                        {article.content}
+                    </p>
+                    <div className={styles["card-meta"]}>
+                        <span className={styles["card-author"]}>
+                            작성자: {article.info.user.name}
+                        </span>
+                        <span className={styles["card-date"]}>
+                            작성일: {article.info.createdAt}
+                        </span>
+                        <span className={styles["card-read-time"]}>
+                            읽는 시간: {article.info.readingTime.value}{article.info.readingTime.unit}
+                        </span>
+                        <span className={styles["card-views"]}>
+                            조회수: {article.data.viewCnt}
+                        </span>
+                        <span className={styles["card-comments"]}>
+                            댓글: {article.data.commentCnt}
+                        </span>
                     </div>
                 </div>
+
+                <ArticleMenu article={article} menuVisible={menuVisible} setMenuVisible={setMenuVisible} />
             </article>
         </Link >
     );
 }
 
-// 더보기 버튼을 누르면 나오는 메뉴
-function ArticleMenu({ article }) {
-    return (
-        <section className={styles['article-menu']} onClick={(event) => {
-            preventEvent(event);
-        }}>
-            {/* 권한 있을 경우에만 수정, 삭제 버튼 나오게 */}
-            <button onClick={(event) => {
-                preventEvent(event);
-                console.log(`edit article ${article.id}`)
-            }}>수정</button>
-            <button onClick={(event) => {
-                preventEvent(event);
-                console.log(`delete article ${article.id}`)
-            }}>삭제</button>
-            <button onClick={(event) => {
-                preventEvent(event);
-                console.log(`share article ${article.id}`)
-            }}>공유</button>
-
-        </section>
-    );
-}
-
-const preventEvent = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-};
 
 export default ArticleCard;
---
+++ src/components/article/ArticleMenu.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import styles from './ArticleMenu.module.css';
+import preventEvent from './utils/preventEvent';
+import { isVisible } from '@testing-library/user-event/dist/utils';
+
+import { ReactComponent as MoreVertIcon } from 'assets/icons/more_vert.svg';
+
+
+function ArticleMenuButton({ menuVisible, setMenuVisible }) {
+    function toggleMenu(event, menuVisible, setMenuVisible) {
+        preventEvent(event);
+        setMenuVisible(!menuVisible);
+    }
+    return (
+        <MoreVertIcon className={styles['article-menu-button']} onClick={(event) => { toggleMenu(event, menuVisible, setMenuVisible) }} />
+    );
+}
+
+// 더보기 버튼을 누르면 나오는 메뉴
+function ArticleMenu({ article, menuVisible, setMenuVisible }) {
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuVisible(false);
+            }
+        };
+
+        if (menuVisible) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuVisible, setMenuVisible]);
+
+    if (!menuVisible) {
+        return null;
+    }
+
+    return (
+        <section ref={menuRef} className={`${styles['article-menu']} ${menuVisible ? 'visible' : 'none'}`} onClick={(event) => {
+            preventEvent(event);
+        }}>
+            {/* 권한 있을 경우에만 수정, 삭제 버튼 나오게 */}
+            <button onClick={(event) => {
+                preventEvent(event);
+                // console.log(`edit article ${article.id}`)
+            }}>수정</button>
+            <button onClick={(event) => {
+                preventEvent(event);
+                // console.log(`delete article ${article.id}`)
+            }}>삭제</button>
+            <button onClick={(event) => {
+                preventEvent(event);
+                // console.log(`share article ${article.id}`)
+            }}>공유</button>
+
+        </section>
+    );
+}
+
+export { ArticleMenuButton, ArticleMenu };
